22e7ce11f02c71075fb34f230c8ce1b28ba8da6e
paragkhamkar <parag.khamkar@cuelogic.com>
2020-01-14 17:16:28 +0530

    Created Basic Components

--- TO-DO-App/src/app/app.module.ts
+++ TO-DO-App/src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
+import { SignupComponent } from './user-auth/signup/signup.component';
+import { LoginComponent } from './user-auth/login/login.component';
+import { UserComponent } from './user/user.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { TodoComponent } from './user/todo/todo.component';
+import { PublicComponent } from './user/todo/public/public.component';
+import { PrivateComponent } from './user/todo/private/private.component';
+import { NewTodoComponent } from './user/todo/new-todo/new-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAuthComponent,
+    SignupComponent,
+    LoginComponent,
+    UserComponent,
+    ProfileComponent,
+    TodoComponent,
+    PublicComponent,
+    PrivateComponent,
+    NewTodoComponent
   ],
   imports: [
     BrowserModule
